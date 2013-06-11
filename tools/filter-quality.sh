5e6670cf75c1035047ef983077c3e6df980a922f
Konrad Rudolph <konrad_rudolph@madrat.net>
2013-06-11 16:12:11 +0100

    Made reaper output FastQ

--- tools/filter-quality.sh
+++ tools/filter-quality.sh
@@ -3,4 +3,4 @@ output="$2"
 
 reaper -i "$input" -basename "$output" \
     -geom no-bc -3pa "" -tabu "" -nnn-check 3/5 -clean-length 16 --bcq-early \
-    -format-clean '>i%i_l%L_t%T%n%C%n'
+    -format-clean '@%I%n%C%n+%n%Q%n'
