77c74243500aa5313b9015c63fbb87ecf00bc8b6
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-10-14 11:59:10 +0100

    Trim reads to common length (30bp), update reaper
    Updated reaper requires explicitly specifying “@” in front of read ID

--- tools/filter-quality.sh
+++ tools/filter-quality.sh
@@ -2,5 +2,5 @@ input="$1"
 output="$2"
 
 reaper -i "$input" -basename "$output" \
-    -geom no-bc -3pa "" -tabu "" -nnn-check 3/5 -clean-length 16 -qqq-check 43/9 \
-    -format-clean '%I%n%C%n+%n%Q%n'
+    -geom no-bc -3pa "" -tabu "" -nnn-check 3/5 -clean-length 16 \
+    -qqq-check 43/9 -trim-length 30 -format-clean '@%I%n%C%n+%n%Q%n'
