1ed49fb8f1a369fed9f48146384f19c68c6e05d4
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-10-14 11:58:43 +0100

    Use proper path settings for tools

--- tools-setup.sh
+++ tools-setup.sh
@@ -1,21 +1,7 @@
-export project_toolpath="/ebi/research/software/Linux_x86_64/bin"
-
 # Use functions instead of aliases so we can export them.
-
-bwa() {
-    "$project_toolpath/bwa" "$@"
-}
-
-samtools() {
-    "$project_toolpath/samtools" "$@"
-}
-
-reaper() {
-    /homes/nenad/local/bin/reaper "$@"
-}
 
 reallocateReads() {
     "$thispath/../scripts/plugins/realloc-reads/reallocateReads" "$@"
 }
 
-export -f bwa samtools reaper reallocateReads
+export -f reallocateReads
